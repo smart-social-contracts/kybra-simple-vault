346691c4ecd0587be55d1147df340902f6f01867
Jose Perez <jmpmonteagudo@gmail.com>
2025-03-25 08:26:17 +0100

    feat: Add vault functionality

---
+++ src/vault/transaction_tracker.py
@@ -0,0 +1,101 @@
+
+def parse_candid(i: str) -> str:
+    i = i.strip()
+    i = i.replace('record {', '{')
+    i = i.replace('opt', '')
+    i = i.replace('blob', '')
+    i = i.replace('vec', '')
+    i = i.replace('principal', '')
+    i = i.replace('null', 'None')
+    i = i.replace(': nat64', '')
+    i = i.replace(': nat', '')
+    i = i.replace(';', '')
+    i = i.replace('_', '')
+    i = i.replace(' = ', ': ')
+    i = i.replace('\x00', '')
+
+    i2 = []
+    is_inside_transactions = False
+    level = 0
+    lines = i.split('\n')
+    for i, l in enumerate(lines):
+        if is_inside_transactions:
+            if '{' in l:
+                level += 1
+            if '}' in l:
+                level -= 1
+                if level < 0:
+                    is_inside_transactions = False
+                    l = ']'
+
+        if '1349681965' in l:
+            continue
+        if '5094982' in l:
+            continue
+        if l == '}' and is_inside_transaction:
+            l = ']'
+        if '{' not in l and '(' not in l:
+            if ',' not in l and i != len(lines) - 1:
+                l += ','
+        if '3331539157:  {' in l:
+            l = '3331539157 : ['
+            is_inside_transactions = True
+            level = 0
+
+        i2.append(l)
+
+    i2 = '\n'.join(i2)
+
+    field_map = {
+        "1_779_015_299": "first_index",
+        "2_799_807_105": "log_length",
+        "3_331_539_157": "transactions",
+        "3_650_848_786": "archived_transactions",
+        "2_131_139_013": "callback",
+        "2_215_343_202": "start",
+        "2_668_074_214": "length",
+        "2781795542": "timestamp",
+        "1191829844": "kind",
+        "3573748184": "amount",
+        "3664621355": "transfer",
+        "1136829802": "from",
+        "947296307": "owner",
+        "25979": "to",
+    }
+
+    for k, v in field_map.items():
+        i2 = i2.replace(k.replace('_', ''), '"%s"' % v)
+
+    print(i2)
+    d = ast.literal_eval(i2)
+
+    return d
+
+
+class TransactionTracker:
+    def __init__(self, principals: list[str]):
+        self.log_length = 0
+        self.transactions = []
+        self.principals = principals
+        self.ledger = {}
+
+    def get_transactions(self):
+        pass
+
+    def parse_transactions(self):
+        pass
+
+    def update_ledger(self):
+        pass
+
+    @property
+    def log_length(self):
+        return self.log_length
+
+    @property
+    def transactions(self):
+        return self.transactions
+
+    @property
+    def ledger(self):
+        return self.ledger
